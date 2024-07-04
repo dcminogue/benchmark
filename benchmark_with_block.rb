dbcf5b91a6eb28081ecc6fcf56b58bfb7df1db7f
dcminogue <danielcminogue@gmail.com>
2024-07-04 16:03:33 -0600

    Add a benchmark method to count time.

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,10 +1,12 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield
+  Time.now - start_time
 end
- 
+
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
-long_string = "apple"*100000000
+long_string = "apple"*1000000
  
 running_time = benchmark { long_string.reverse }
  
